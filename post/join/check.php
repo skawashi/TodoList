f774d6ab026a1b502883477abf6431cfda3b9dce
kawashima <mmyonmmyon@icloud.com>
2022-07-21 15:22:01 +0900

    [/post/join/check.php] モックの作成(cssは未作成)

---
+++ post/join/check.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Title</title>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="page-header">
+        <div class="header-wrapper">
+            <p>TodoList</p>
+            <nav>
+                <ul class="main-nav">
+                    <li>ログイン</li>
+                    <li>会員登録</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+    <form action="" method="POST">
+            <div>
+                <p>以下の内容で登録します</p>
+            </div>
+            <dl>
+                <dt>ユーザーネーム</dt>
+                <dd></dd>
+                <dt>メールアドレス</dt>
+                <dd></dd>
+                <dt>パスワード</dt>
+                <dd>【表示されません】</dd>
+            </dl>
+            <div>
+                <input type="submit" value="登録する">
+            </div>
+        </form>
+    </main>
+
+    <footer class="page-footer footer-wrapper">
+        <p><small>&copy; 2022 TODO_LIST</small></p>
+    </footer>
+
+</body>
+</html>
