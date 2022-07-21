568674950ec4ab99aee34892107a870518e538d3
kawashima <mmyonmmyon@icloud.com>
2022-07-21 16:40:50 +0900

    [/post/join/index.php]入力のエラーチェック実装

--- post/join/index.php
+++ post/join/index.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+
+// フォームのエラーチェック
+if(!empty($_POST)) {
+    if($_POST['name'] == '') {
+        $error['name'] = 'blank';
+    }
+
+    if($_POST['mail'] == '') {
+        $error['mail'] = 'blank';
+    }
+
+    if($_POST['password'] == '') {
+        $error['password'] = 'blank';
+    } elseif(strlen($_POST['password']) < 4) {
+        $error['password'] = 'length';
+    }
+
+    if(empty($error)) {
+        $_SESSION['join'] = $_POST;
+        header('Location: ./check.php');
+        exit();
+    }
+}
+?>
+
 <!doctype html>
 <html lang="ja">
 
@@ -31,11 +58,29 @@
             </div>
             <dl>
                 <dt>ユーザーネーム</dt>
-                <dd><input type="text" name="name" class="input-size"></dd>
+                <dd>
+                    <input type="text" name="name" class="input-size">
+                    <?php if($error['name'] == 'blank'): ?>
+                        <p class="error">ユーザーネームを入力してください</p>
+                    <?php endif; ?>
+                </dd>
                 <dt>メールアドレス</dt>
-                <dd><input type="text" name="mail" class="input-size"></dd>
+                <dd>
+                    <input type="text" name="mail" class="input-size">
+                    <?php if($error['mail'] == 'blank'): ?>
+                        <p class="error">メールアドレスを入力してください</p>
+                    <?php endif; ?>
+                </dd>
                 <dt>パスワード</dt>
-                <dd><input type="password" name="password" class="input-size"></dd>
+                <dd>
+                    <input type="password" name="password" class="input-size">
+                    <?php if($error['password'] == 'blank'): ?>
+                        <p class="error">パスワードを入力してください</p>
+                    <?php endif; ?>
+                    <?php if($error['password'] == 'length'): ?>
+                        <p class="error">パスワードは4文字以上で設定してください</p>
+                    <?php endif; ?>
+                </dd>
             </dl>
             <div class="submit">
                 <input type="submit" value="登録する" class="button">
