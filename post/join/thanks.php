c1c9638ef453de89bb62d9c396b66625503de60e
kawashima <mmyonmmyon@icloud.com>
2022-07-21 18:15:14 +0900

    [post/join/thanks.php]モックおよびログインへのリンクを作成

---
+++ post/join/thanks.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Title</title>
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<body>
+    <header class="page-header">
+        <div class="header-wrapper">
+            <p>TodoList</p>
+            <nav>
+                <ul class="main-nav">
+                    <li><a href="../index.php">ログイン</a></li>
+                    <li><a href="./index.php">会員登録</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <div>
+            <p>登録ありがとうございます</p>
+            <p><a href="../index.php">&laquo;&nbsp;ログイン</a></p>
+        </div>
+    </main>
+
+    <footer class="page-footer footer-wrapper">
+        <p><small>&copy; 2022 TODO_LIST</small></p>
+    </footer>
+</body>
+</html>
