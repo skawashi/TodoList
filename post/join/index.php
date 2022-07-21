0fb5cf02f9111c3d157cf77fcf05ac51ae0e0b02
kawashima <mmyonmmyon@icloud.com>
2022-07-21 18:27:03 +0900

    [/post/join/index.php]メールアドレスの重複チェックを実装

--- post/join/index.php
+++ post/join/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require('../dbconnect.php');
 
 // フォームのエラーチェック
 if(!empty($_POST)) {
@@ -17,14 +18,32 @@ if(!empty($_POST)) {
         $error['password'] = 'length';
     }
 
+    $member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email = ?');
+    $member->execute(array($_POST['mail']));
+    $record = $member->fetch();
+    if($record['cnt'] > 0) {
+        $error['mail'] = 'duplicate';
+    }
+
     if(empty($error)) {
         $_SESSION['join'] = $_POST;
         header('Location: ./check.php');
         exit();
     }
 }
+
+if($_GET['action'] == 'rewrite') {
+    $_POST = $_SESSION['join'];
+    // 現在は必要ないが、画像を登録してもらう際はもう一度指定してもらう必要があるのでそのフラグ用
+    $error['rewrite'] = 'true';
+}
 ?>
 
+<?php
+    function hsc($value) {
+        return htmlspecialchars($value, ENT_QUOTES);
+    }
+?>
 <!doctype html>
 <html lang="ja">
 
@@ -44,8 +63,8 @@ if(!empty($_POST)) {
             <p>TodoList</p>
             <nav>
                 <ul class="main-nav">
-                    <li>ログイン</li>
-                    <li>会員登録</li>
+                    <li><a href="../index.php">ログイン</a></li>
+                    <li><a href="./index.php">会員登録</a></li>
                 </ul>
             </nav>
         </div>
@@ -59,26 +78,29 @@ if(!empty($_POST)) {
             <dl>
                 <dt>ユーザーネーム</dt>
                 <dd>
-                    <input type="text" name="name" class="input-size">
+                    <input type="text" name="name" size="35" maxlength="255" value="<?php echo hsc($_POST['name']); ?>" class="input-size">
                     <?php if($error['name'] == 'blank'): ?>
-                        <p class="error">ユーザーネームを入力してください</p>
+                        <p class="error">* ユーザーネームを入力してください</p>
                     <?php endif; ?>
                 </dd>
                 <dt>メールアドレス</dt>
                 <dd>
-                    <input type="text" name="mail" class="input-size">
+                    <input type="text" name="mail" size="35" maxlength="255" value="<?php echo hsc($_POST['mail']) ?>" class="input-size">
                     <?php if($error['mail'] == 'blank'): ?>
-                        <p class="error">メールアドレスを入力してください</p>
+                        <p class="error">* メールアドレスを入力してください</p>
+                    <?php endif; ?>
+                    <?php if($error['mail'] == 'duplicate'): ?>
+                        <p>* このメールアドレスは既に登録されています</p>
                     <?php endif; ?>
                 </dd>
                 <dt>パスワード</dt>
                 <dd>
-                    <input type="password" name="password" class="input-size">
+                    <input type="password" name="password" size="10" maxlength="100" value="<?php echo hsc($_POST['password']); ?>" class="input-size">
                     <?php if($error['password'] == 'blank'): ?>
-                        <p class="error">パスワードを入力してください</p>
+                        <p class="error">* パスワードを入力してください</p>
                     <?php endif; ?>
                     <?php if($error['password'] == 'length'): ?>
-                        <p class="error">パスワードは4文字以上で設定してください</p>
+                        <p class="error">* パスワードは4文字以上で設定してください</p>
                     <?php endif; ?>
                 </dd>
             </dl>
