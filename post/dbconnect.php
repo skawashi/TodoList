1a8ddac6a6cead583cafe6b4443e970320500ebb
kawashima <mmyonmmyon@icloud.com>
2022-07-28 02:29:21 +0900

    SQLインジェクション対策のオプション設定を追加

--- post/dbconnect.php
+++ post/dbconnect.php
@@ -1,6 +1,11 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=todolist; host=localhost;charset=utf8', 'root', 'root');
+    $opt = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+        PDO::ATTR_EMULATE_PREPARES => false
+    );
+    $db = new PDO('mysql:dbname=todolist; host=localhost;charset=utf8', 'root', 'root', $opt);
 } catch(PDOException $e) {
     echo 'DB接続エラー : ' . $e->getMessage();
 }
